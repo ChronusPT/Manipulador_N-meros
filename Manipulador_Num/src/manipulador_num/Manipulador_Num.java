e6a9f9580b88c247b901e1d8c2d86e6bddffa35f
Diogo Bugalho <wheretheycry@hotmail.com>
2022-09-19 15:45:08 +0100

    Função - Decrescente

--- Manipulador_Num/src/manipulador_num/Manipulador_Num.java
+++ Manipulador_Num/src/manipulador_num/Manipulador_Num.java
@@ -23,19 +23,29 @@ public class Manipulador_Num {
                 case 1 :
                     levalores ("Insira um valor","Insira outro valor");
                     MaiorNumero.maior2(x,y);
-                case 2: le1valor("Insira um valor : ");
-                    NumeroDecrescente.ordenadecrescente(x);
                     break;
-                default: System.out.println("Insira Opção válida!");  
+                case 2: 
+                    do {
+                        le1valor("Insira um valor : ");
+                    }while (x <= 0);
+                    NumeroDecrescente.ordenadecrescente(x); 
+                    break;
+                    
+                default: System.out.println("Insira Opção válida!");
             }
     }while(op!=0);
-                }
+        }
     private static void levalores(String s1, String s2) {
         System.out.println(s1);
         x = ler.nextInt();
         System.out.println(s2);
         y = ler.nextInt();
      
+    }
+
+    private static void le1valor(String s1) {
+       System.out.println(s1);
+        x = ler.nextInt();
     }
     
 }
---
+++ Manipulador_Num/src/manipulador_num/NumeroDecrescente.java
@@ -0,0 +1,13 @@
+
+package manipulador_num;
+
+
+class NumeroDecrescente {
+
+    static void ordenadecrescente(int x) {
+        for(int i = x; i >= 0; i--){
+            System.out.println(i);
+        }
+    }
+    
+}
