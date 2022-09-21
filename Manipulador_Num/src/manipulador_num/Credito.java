f3727141e29ceadadba4ee9a99a8c5f1fede6256
Diogo Bugalho <wheretheycry@hotmail.com>
2022-09-21 10:06:06 +0100

    Função cred (corr)*

--- Manipulador_Num/src/manipulador_num/Credito.java
+++ Manipulador_Num/src/manipulador_num/Credito.java
@@ -6,40 +6,27 @@ class Credito {
 
     static void calculaSaldo() {
         
-        int saldoA = 0;
-        int op = 0;
-        double credito;
-        
+        int saldoA;
+        double credito = 0; 
+       
         System.out.println("Qual o valor do rendimento anual ? ");
         saldoA = Manipulador_Num.ler.nextInt();
         
-        credito = 0;
+        if (saldoA < 201)
+            credito = (saldoA * 0);
+ 
+        else if (saldoA > 200 && saldoA < 401)
+            credito = (saldoA * 0.20);
+              
+        else if (saldoA > 400 && saldoA < 601)
+            credito = (saldoA * 0.30);
         
-        if (saldoA > 201 && saldoA < 400)
-            op = 1;
-        else if (saldoA > 401 && saldoA < 600)
-            op = 2;
-        else if (saldoA > 601 )
-            op = 3;
-        else (saldoA <= 200)
-            op = 4;
-                
-        switch(saldoA) {
-            case 1:
-                    credito = saldoA * 0.20;
-                break;
-            case 2:
-                    credito = saldoA * 0.30;
-                break;
-            case 3:
-                if (saldoA > 601 )
-                    credito = saldoA * 0.40;   
-                break;
-            default:
-                    credito = saldoA * 0;    
+        else if (saldoA > 600 )
+            credito = (saldoA * 0.40);
+                        
         
-        System.out.println("O valor do credito é de " +credito);
-        }
+        System.out.println("O valor do credito atribuido é do Tipo " +tier+
+                " e tem o valor de " +credito);
         
     }
          
